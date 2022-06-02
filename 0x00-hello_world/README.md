c57ef0b0aa5702251903ed85821b28ab241ae65e
Endy50 <endyorefi@gmail.com>
2022-06-02 16:17:07 -0700

    Preprocess a source code and save output

---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+This runs a source code through the preprocessor and saves the output as input for 'c'
---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+  printf ("This string says %c.\n", M);
+  return (0);
+}

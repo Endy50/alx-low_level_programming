9b94fa284ee1f322d6b7a9aba8290d52f02b028d
Endy50 <endyorefi@gmail.com>
2022-06-14 07:45:22 -0700

    puts

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main- Entry point
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	printf("The size of a float is: %i \n", sizeof(float));
+	printf("The size of a char is: %i \n", sizeof(char));
+	printf("The size of an int is: %i \n", sizeof(int));
+	printf("The size of a long int is: %i \n", sizeof(long_int));
+	printf("The size of a long long int is: %i \n". sizeof(long_long_int));
+	return (0);
+}

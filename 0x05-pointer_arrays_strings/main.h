a1e11a9b1dacda944e5acbb723f615703d7a5c5c
Endy50 <endyorefi@gmail.com>
2022-06-15 11:05:16 -0700

    reset value

---
+++ 0x05-pointer_arrays_strings/main.h
@@ -0,0 +1,20 @@
+/**
+ *main.h - custom header file
+ *
+ *description: accepts prototypes and declaration of functions
+ *
+ */
+
+int _putchar(char c);
+
+void reset_to_98(int *n);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);

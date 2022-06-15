4f97276d96952e5d6133060ec7d91a219c3e4368
Endy50 <endyorefi@gmail.com>
2022-06-15 06:11:16 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,5 @@
-#include "main.h" 
+#include "main.h"
 /**
- *main.h- header file
- *
  *main- Entry point
  *
  *Return: 0
@@ -10,8 +8,9 @@
 int main(void)
 {
 	int i;
-	
+
 	char c[] = "_putchar";
+
 	i = 0;
 
 	while (c[i] != '\0')
